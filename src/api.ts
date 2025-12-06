6d93617c6d57d697f1d0cc701387f232c1dc18f5
Joel Gerson Cori Chavez <joelcori111@gmail.com>
2025-12-05 19:55:04 -0500

    se arreglo los cors

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://cbf616740ae6.ngrok-free.app/api/Paciente",
+  baseURL: "https://ba53e78792a3.ngrok-free.app/api/Paciente",
+  withCredentials: true,
   headers: {
     "ngrok-skip-browser-warning": "true"
   }
