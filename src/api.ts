d4069dbc5c035067fcab28a267037d476e6621a6
Joel Gerson Cori Chavez <joelcori111@gmail.com>
2025-12-05 20:17:59 -0500

    se quito las credenciales

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://9cd9549c9d02.ngrok-free.app/api/Paciente",
-  withCredentials: true,
   headers: {
     "ngrok-skip-browser-warning": "true"
   }
