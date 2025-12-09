965fbca804a464103aee8035bd11c787848ff1f2
Joel Gerson Cori Chavez <joelcori111@gmail.com>
2025-12-08 20:24:23 -0500

    se agrego ssl a backend

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://backendpacientesapi.somee.com/api/Paciente",
+  baseURL: "https://backendpacientesapi.somee.com/api/Paciente/Lista/api/Paciente",
   headers: {
     "ngrok-skip-browser-warning": "true"
   }
